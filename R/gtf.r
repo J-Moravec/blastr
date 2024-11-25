9a315f81a821717c6d6db73cc4f545511edb0df7
Jiří Moravec <jiri.c.moravec@gmail.com>
2024-11-25 16:13:27 +1300

    (micro) optimized version

--- R/gtf.r
+++ R/gtf.r
@@ -40,12 +40,12 @@
 #' Definition of GTF 2.2: http://mblab.wustl.edu/GTF22.html
 read_gtf = function(x, feature = NULL, attributes = FALSE){
     # gtf contains comments within fields as well
-    y = readLines(x)
-    y = y |> grep(pattern = "^#", value = TRUE, invert = TRUE)
-    y = scan(text = y, sep = "\t", quote = "", what = rep(list(character()), 9), quiet = TRUE)
+    y = scan(x, sep = "\t", quote = "", what = rep(list(character()), 9),
+             comment.char = "", fill = TRUE, quiet = TRUE)
     names(y) = c("chr", "source", "feature", "start", "end",
                  "score", "strand", "frame", "attribute")
     y = data.frame(y)
+    y = y[!startsWith(y[[1]], "#"),]
 
     if(!is.null(feature))
         y = y[y$feature %in% feature,]
@@ -61,35 +61,32 @@ read_gtf = function(x, feature = NULL, attributes = FALSE){
 #' @export
 parse_attributes = function(x){
     y = strsplit(x, "\"; ", fixed = TRUE)
-    y = lapply(y, parse_attribute_fields)
+    for(i in seq_along(y)){
+        z = y[[i]] |> strfsplit(pattern = " ", fixed = TRUE)
+        y[[i]] = aggregate(z[[2]], z[[1]])
+        }
     y = rbindfill(y)
-    y[] = lapply(y, gsub, pattern = "\"", replacement = "")
-    y
-    }
-
-
-parse_attribute_fields = function(x, sep = " "){
-    s = strfsplit(x, pattern = sep, fixed = TRUE)
-    aggregate(s[[2]], s[[1]])
+    y = gsub(y, pattern = "\"", replacement = "", fixed = TRUE)
+    as.data.frame(y)
     }
 
 
 rbindfill = function(x){
-    fill = function(x, names){
-        x[setdiff(names, names(x))] = NA
-        x
+   fill = function(x, names){
+        y = rep(NA_character_, length(names))
+        names(y) = names
+        y[names(x)] = x
+        y
         }
 
     nm = lapply(x, names) |> unlist() |> unique()
-    x = lapply(x, fill, names = nm)
-    do.call(rbind.data.frame, x)
+    vapply(x, fill, character(length(nm)), names = nm) |> t()
     }
 
 
 aggregate = function(x, by){
-    if(length(x) != length(by))
-        stop("x and by must have the same length!")
-
     u = unique(by)
-    lapply(u, \(y) paste0(x[by == y], collapse = "; ")) |> stats::setNames(u)
+    y = vapply(u, \(y) paste0(x[by == y], collapse = "; "), character(1))
+    names(y) = u
+    y
     }
